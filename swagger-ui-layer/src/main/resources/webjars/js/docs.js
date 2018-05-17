d88e9205b3e9b2375f8a605dfaa3d9df7bbf23d5
李明新 <limingxin@zillionfortune.com>
2018-05-17 11:07:24 +0800

    增加json格式debug,支持req和resp的json化结构

--- swagger-ui-layer/src/main/resources/webjars/js/docs.js
+++ swagger-ui-layer/src/main/resources/webjars/js/docs.js
@@ -1,141 +1,166 @@
 $.views.settings.allowCode(true);
-$.views.converters("getResponseModelName", function(val) {
-  return getResponseModelName(val);
+$.views.converters("getResponseModelName", function (val) {
+    return getResponseModelName(val);
 });
 
 var tempBody = $.templates('#temp_body');
 var tempBodyResponseModel = $.templates('#temp_body_response_model');
+var swaggerData = {};
 
 //获取context path
 var contextPath = getContextPath();
 function getContextPath() {
-  var pathName = document.location.pathname;
-  var index = pathName.substr(1).indexOf("/");
-  var result = pathName.substr(0,index+1);
-  return result;
+    var pathName = document.location.pathname;
+    var index = pathName.substr(1).indexOf("/");
+    var result = pathName.substr(0, index + 1);
+    return result;
 }
 
-$(function(){
+$(function () {
     $.ajax({
-        url : "v2/api-docs",
+        url: "v2/api-docs",
 // 	        url : "http://petstore.swagger.io/v2/swagger.json",
-        dataType : "json",
-        type : "get",
-        async : false,
-        success : function(data) {
+        dataType: "json",
+        type: "get",
+        async: false,
+        success: function (data) {
             //layui init
-            layui.use([ 'layer','jquery', 'element' ], function() {
-	            var $ = layui.jquery, layer = layui.layer, element = layui.element;
-	        });
+            layui.use(['layer', 'jquery', 'element'], function () {
+                var $ = layui.jquery, layer = layui.layer, element = layui.element;
+            });
             var jsonData = eval(data);
-            
+            swaggerData = jsonData;
             $("#title").html(jsonData.info.title);
             $("body").html($("#template").render(jsonData));
-            
-            $("[name='a_path']").click(function(){
-	            var path = $(this).attr("path");
-	            var method = $(this).attr("method");
-	            var operationId = $(this).attr("operationId");
-	            $.each(jsonData.paths[path],function(i,d){
-	              if(d.operationId == operationId){
-                      d.path = path;
-                      d.method = method;
-		              $("#path-body").html(tempBody.render(d));
-                      var modelName = getResponseModelName(d.responses["200"]["schema"]["$ref"]);
-                      if(modelName){
-                        $("#path-body-response-model").html(tempBodyResponseModel.render(jsonData.definitions[modelName]));
-                      }
-	              }
-	            });
-	        });
-	        
-	       //提交测试按钮
-	       $("[name='btn_submit']").click(function(){
+
+            $("[name='a_path']").click(function () {
+                var path = $(this).attr("path");
+                var method = $(this).attr("method");
+                var operationId = $(this).attr("operationId");
+                $.each(jsonData.paths[path], function (i, d) {
+                    if (d.operationId == operationId) {
+                        d.path = path;
+                        d.method = method;
+                        $("#path-body").html(tempBody.render(d));
+                        var modelName = getResponseModelName(d.responses["200"]["schema"]["$ref"]);
+                        if (modelName) {
+                            $("#path-body-response-model").html(tempBodyResponseModel.render(jsonData.definitions[modelName]));
+                        }
+                    }
+                });
+            });
+
+            //提交测试按钮
+            $("[name='btn_submit']").click(function () {
                 var operationId = $(this).attr("operationId");
                 var parameterJson = {};
-                $("input[operationId='"+operationId+"']").each(function(index, domEle){
+                $("input[operationId='" + operationId + "']").each(function (index, domEle) {
                     var k = $(domEle).attr("name");
                     var v = $(domEle).val();
-                    parameterJson.push({k:v});
+                    parameterJson.push({k: v});
                 });
             });
         }
     });
-    
+
 });
 
 
-function getResponseModelName(val){
-  if(!val){
-    return null;
-  }
-  return val.substring(val.lastIndexOf("/")+1,val.length);
+function getResponseModelName(val) {
+    if (!val) {
+        return null;
+    }
+    return val.substring(val.lastIndexOf("/") + 1, val.length);
 }
 
 //测试按钮，获取数据
-function getData(operationId){
-   var path = contextPath + $("[m_operationId='"+operationId+"']").attr("path");
-   //path 参数
-   $("[p_operationId='"+operationId+"'][in='path']").each(function(index, domEle){
-       var k = $(domEle).attr("name");
-       var v = $(domEle).val();
-       if(v){
-           path = path.replace("{"+k+"}",v);
-       }
-   });
-   
-   //请求方式
-   var parameterType = $("#content_type_"+operationId).val();
-   
-   //query 参数
-   var parameterJson = {};
-   if("form" == parameterType){
-       $("[p_operationId='"+operationId+"'][in='query']").each(function(index, domEle){
-           var k = $(domEle).attr("name");
-           var v = $(domEle).val();
-           if(v){
-               parameterJson[k] = v;
-           }
-       });
-   }else if("json" == parameterType){
-       var str = $("#text_tp_"+operationId).val();
-       try{
-           parameterJson = JSON.parse(str); 
-       }catch(error){
-           layer.msg(""+error,{icon:5});
-           return false;
-       }
-   }
-   
-   //发送请求
-   $.ajax({
-	   type: $("[m_operationId='"+operationId+"']").attr("method"),
-	   url: path,
-	   data: parameterJson,
-	   dataType: 'json',
-	   success: function(data){
-	     var options = {
-          withQuotes: true
-         };
-	     $("#json-response").jsonViewer(data, options);
-	   }
-   });
+function getData(operationId) {
+    var path = contextPath + $("[m_operationId='" + operationId + "']").attr("path");
+    //path 参数
+    $("[p_operationId='" + operationId + "'][in='path']").each(function (index, domEle) {
+        var k = $(domEle).attr("name");
+        var v = $(domEle).val();
+        if (v) {
+            path = path.replace("{" + k + "}", v);
+        }
+    });
+
+    //请求方式
+    var parameterType = $("#content_type_" + operationId).val();
+
+    //query 参数
+    var parameterJson = {};
+    if ("form" == parameterType) {
+        $("[p_operationId='" + operationId + "'][in='query']").each(function (index, domEle) {
+            var k = $(domEle).attr("name");
+            var v = $(domEle).val();
+            if (v) {
+                parameterJson[k] = v;
+            }
+        });
+    } else if ("json" == parameterType) {
+        var str = $("#text_tp_" + operationId).val();
+        try {
+            parameterJson = JSON.parse(str);
+        } catch (error) {
+            layer.msg("" + error, {icon: 5});
+            return false;
+        }
+    }
+
+    //发送请求
+    $.ajax({
+        type: $("[m_operationId='" + operationId + "']").attr("method"),
+        url: path,
+        data: JSON.stringify(parameterJson),
+        dataType: 'json',
+        contentType: "application/json",
+        success: function (data) {
+            var options = {
+                withQuotes: true
+            };
+            $("#json-response").jsonViewer(data, options);
+        }
+    });
 }
 
 
 //请求类型
-function changeParameterType(el){
+function changeParameterType(el) {
     var operationId = $(el).attr("operationId");
     var type = $(el).attr("type");
-    $("#content_type_"+operationId).val(type);
+    $("#content_type_" + operationId).val(type);
     $(el).addClass("layui-btn-normal").removeClass("layui-btn-primary");
-    if("form" == type){
-        $("#text_tp_"+operationId).hide();
-        $("#table_tp_"+operationId).show();
-        $("#pt_json_"+operationId).addClass("layui-btn-primary").removeClass("layui-btn-normal");
-    }else if("json" == type){
-       $("#text_tp_"+operationId).show();
-       $("#table_tp_"+operationId).hide();
-       $("#pt_form_"+operationId).addClass("layui-btn-primary").removeClass("layui-btn-normal");
+    if ("form" == type) {
+        $("#text_tp_" + operationId).hide();
+        $("#text_tp_val_" + operationId).hide();
+        $("#table_tp_" + operationId).show();
+        $("#pt_json_" + operationId).addClass("layui-btn-primary").removeClass("layui-btn-normal");
+    } else if ("json" == type) {
+        $("#text_tp_" + operationId).show();
+        $("#text_tp_val_" + operationId).show();
+        var options = {
+            withQuotes: true
+        };
+        var body = $(".reqBody").text();
+        if (body != "") {
+            var bodyParam = body.substring(body.lastIndexOf("/") + 1, body.length);
+            $("#text_tp_val_" + operationId).jsonViewer(reqResolver(bodyParam, {}), options);
+        }
     }
+    $("#pt_form_" + operationId).addClass("layui-btn-primary").removeClass("layui-btn-normal");
+}
+
+function reqResolver(req, jsonData) {
+    var obj = swaggerData.definitions[req]["properties"];
+    //循环对象，如果key为obj继续循环。
+    for (var key in obj) {
+        if (typeof obj[key] == "object" && obj[key].type == "object") {
+            reqResolver(obj[key], jsonData);
+        } else {
+            jsonData[key] = obj[key].type;
+        }
+    }
+    //返回最终的请求对象
+    return jsonData;
 }
